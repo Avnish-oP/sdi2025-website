5aba272f36793b4458d3995c8542124f715cf914
Avnish-oP <kavnish1245@gmail.com>
2024-12-20 22:10:40 +0530

    eslint rule error fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import AboutHackathon from "@/components/AboutUs";
 import HeroSection from "@/components/Hero";
-import Navbar from "@/components/Navbar";
 import { HackathonTimeline } from "@/components/Schedule";
 import  Themes  from "@/components/Themes";
 import WhySDISection from "@/components/Why";
