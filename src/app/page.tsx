a8559d0cb3259ad90b90d997079ff350f326ab8e
bimlesharma <bimlesh.mdb@gmail.com>
2024-12-21 21:01:24 +0530

    conflict resolution

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,19 +4,22 @@ import Navbar from "@/components/Navbar";
 import { HackathonTimeline } from "@/components/Schedule";
 import  Themes  from "@/components/Themes";
 import  Tracks  from "@/components/Tracks";
-import OrganizingCommittee from "@/components/Committee";
+import Committee from "@/components/Committee";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
     <Navbar />
-    <HeroSection />
-    <AboutHackathon />
-    {/* <Themes /> */}
-    <Tracks />
-    <HackathonTimeline />
-    <OrganizingCommittee />
+
+    <div id="home"><HeroSection /></div>
+    <div id="about"><AboutHackathon /></div>
+    <div id="themes">
+      {/* <Themes /> */}
+      <Tracks />
+    </div>
+    <div id="schedule"><HackathonTimeline /></div>
+    <Committee />
     </>
   );
 }
