a518b9a4b6347ecb5da9f6b2441a62ace3093c2e
bimlesharma <125790247+bimlesharma@users.noreply.github.com>
2024-12-23 12:34:42 +0000

    eslint error fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import AboutHackathon from "@/components/AboutUs";
 import HeroSection from "@/components/Hero";
 import { HackathonTimeline } from "@/components/Schedule";
 // import  Themes  from "@/components/Themes";
-// import WhySDISection from "@/components/Why";
+import WhySDISection from "@/components/Why";
 // import Committee from "@/components/Committee";
 import Tracks from "@/components/Tracks";
 
