fe63d186668a3f2d7b8fd6b628c338f33c13637f
Avnish-oP <130829068+Avnish-oP@users.noreply.github.com>
2025-01-11 06:07:39 +0000

    faq update

--- src/app/faqs/page.tsx
+++ src/app/faqs/page.tsx
@@ -15,7 +15,7 @@ const faqs = [
   { question: "How does this challenge promote collaboration?", answer: "The event fosters collaboration among policymakers, technologists, academia, and students to address systemic issues collectively." },
   { question: "Who can participate in the challenge?", answer: "Students from universities and colleges ranked under the NIRF 2024 framework and located within Delhi (NCT) are eligible." },
   // { question: "How many entries can an institution submit per theme?", answer: "Each institution can submit a maximum of two entries per theme, finalized through an internal screening process." },
-  { question: "How are submissions made?", answer: "All submissions must be uploaded by the institution’s designated Single Point of Contact (SPOC) via the official competition portal." },
+  { question: "How are submissions made?", answer: "All submissions must be uploaded by team leader." },
   { question: "How many members are allowed in a team?", answer: "Each team must consist of three to five members, all from the same college." },
   { question: "What are the themes for the challenge?", answer: "The themes focus on addressing Delhi&apos;s urban challenges: Traffic Management, Zero Waste, Cleaner Air, Water Efficiency, and Women’s Safety." },
   { question: "Can a single submission address multiple themes?", answer: "Submissions should primarily focus on one theme to maintain clarity and relevance, but interdisciplinary approaches are welcome if they enhance the solution." },
